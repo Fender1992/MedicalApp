3145b6566a69f1eac169ac561b65ccafcd0309a4
Rolo <rolandofender@gmail.com>
2024-05-02 22:52:29 -0500

    Auth added through mobile app

--- MedicalAppAPI/Program.cs
+++ MedicalAppAPI/Program.cs
@@ -19,6 +19,26 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("DevCors", (corsBuilder) =>
+    {
+        corsBuilder.WithOrigins("http://localhost:4200", "http://localhost:3000",
+            "http://localhost:8000", "https://0bb9-2600-8803-2b10-9200-d956-739-821f-b9e4.ngrok-free.app/api/Auth/Login")
+        .AllowAnyMethod()
+        .AllowAnyHeader()
+        .AllowCredentials();
+    });
+    options.AddPolicy("ProdCors", (corsBuilder) =>
+    {
+        corsBuilder.WithOrigins("https://medicalAppServices.com")
+        .AllowAnyMethod()
+        .AllowAnyHeader()
+        .AllowCredentials();
+    });
+});
+
 builder.Services.AddSwaggerGen(options =>
 {
     options.SwaggerDoc("v1", new OpenApiInfo {Title = "Medical App API", Version = "v1"});
@@ -97,11 +117,16 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseCors("DevCors");
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+else
+{
+    app.UseCors("ProdCors");
+    app.UseHttpsRedirection();
+}
 
-app.UseHttpsRedirection();
 
 app.UseAuthentication();
 
